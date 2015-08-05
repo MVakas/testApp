bdd62e4a24a64ddd52c4224cf0124500e1e05064
Vakas <vakas@me.com>
2015-08-05 10:12:55 +0500

    Initial Commit

---
+++ GitHubProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubProject
+//
+//  Created by Vakas Zia on 05/08/2015.
+//  Copyright (c) 2015 VeeSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
